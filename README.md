70b1acb2838402135e6c8ca46a0c359572b84c6c
Robert J Martin <robertmartinbc@shaw.ca>
2013-07-31 23:23:43 -0700

    First commit.

---
+++ README.md
@@ -0,0 +1,5 @@
+Welcome to MobileWRX!
+
+This site will be a phenomenon!
+
+The first site to offer QR coded marketing for Smart Phones.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mobilewrx::Application.config.secret_token = 'e2fc1e4d0cb1d2ff3d01a1c7138d83db7c5d1621e384474e956591ea272fabaf38672e54e945946d3874d0d8359cb18ed60f0da2b6d0b29b743ba32c2248cf4c'
