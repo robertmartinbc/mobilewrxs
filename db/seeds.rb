8639d9abadb4c7fbce30c1df6396fa656d3811b3
Robert J Martin <robertmartinbc@shaw.ca>
2013-08-01 00:29:05 -0700

    Installed Faker and seeded the database.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'faker'
+
+rand(10..30).times do
+  p = Promotion.create(title: Faker::Lorem.words(rand(1..10)).join(" "), body: Faker::Lorem.paragraphs(rand(1..4)).join("\n"))
+  rand(3..10).times do
+    p.offers.create(body: Faker::Lorem.paragraphs(rand(1..2)).join("\n"))
+  end
+end
+
+puts "Seed finished"
+puts "#{Promotion.count} posts created"
+puts "#{Offer.count} comments created"
