664e2ed7df62d03fd629bcb886cef9a1074393c6
Robert J Martin <robertmartinbc@shaw.ca>
2013-08-02 20:45:11 -0700

    Added create and update CRUD actions

--- app/controllers/promotions_controller.rb
+++ app/controllers/promotions_controller.rb
@@ -10,8 +10,32 @@ class PromotionsController < ApplicationController
   end
 
   def new
+    @promotion = Promotion.new
   end
 
+  def create
+  @promotion = Promotion.new(params[:promotion])
+  if @promotion.save
+    flash[:notice] = "Promotion was saved."
+    redirect_to @promotion
+  else
+    flash[:error] = "There was an error saving the promotion. Please try again."
+    render :new
+  end
+end
+
   def edit
+    @promotion = Promotion.find(params[:id])
+  end
+
+  def update
+    @promotion = Promotion.find(params[:id])
+    if @promotion.update_attributes(params[:post])
+      flash[:notice] = "Promotion was updated."
+      redirect_to @promotion
+    else
+      flash[:error] = "There was an error saving the promotion. Please try again."
+      render :new
+    end
   end
 end
