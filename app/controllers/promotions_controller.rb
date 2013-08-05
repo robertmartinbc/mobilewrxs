c0bf3bd16b13a00114b1d1927f886927fd6eabd0
Robert J Martin <robertmartinbc@shaw.ca>
2013-08-05 15:52:28 -0700

    Added Pagination.

--- app/controllers/promotions_controller.rb
+++ app/controllers/promotions_controller.rb
@@ -1,6 +1,6 @@
 class PromotionsController < ApplicationController
   def index
-    @promotions = Promotion.all
+    @promotions = Promotion.paginate(page: params[:page], per_page: 8)
   end
 
   def show
